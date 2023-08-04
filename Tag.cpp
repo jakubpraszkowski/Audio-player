90e93785c9c0c1e6aaf62207bf5204167dee5295
Jakub Praszkowski <kubapraszkowski@gmail.com>
2023-08-04 22:10:47 +0200

    add: tag read WIP

---
+++ Tag.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kubuski on 04.08.23.
+//
+
+#include "Tag.h"
---
+++ Tag.h
@@ -0,0 +1,14 @@
+//
+// Created by kubuski on 04.08.23.
+//
+
+#ifndef MUSICLIBRARY_TAG_H
+#define MUSICLIBRARY_TAG_H
+
+
+class Tag {
+
+};
+
+
+#endif //MUSICLIBRARY_TAG_H
